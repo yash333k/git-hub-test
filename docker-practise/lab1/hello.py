499788839244a7b45098f4c1f7f9895c4f0daf2b
root <root@ip-172-31-28-235.us-east-2.compute.internal>
2020-08-04 19:06:18 +0000

    added dockerfile jenkinsfile

---
+++ docker-practise/lab1/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hi')
+
+def hello_world():
+	return 'this is python web app'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
